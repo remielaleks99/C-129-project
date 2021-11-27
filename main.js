ec05c2accc4a40a81a78a219a2fb99d6eb463f75
remielaleks99 <84626020+remielaleks99@users.noreply.github.com>
2021-11-27 13:46:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song = "";
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet is intialized");
+}
+function gotPoses(results){
+    if(results.length > 0){
+      console.log(results);
+      rightWristX = results[0].pose.rightWrist.x;
+      rightWristY = results[0].pose.rightWrist.y;
+      console.log("rightWristX = "+ rightWristX + "rightWristY = " + rightWristY);
+      leftWristX = results[0].pose.leftWrist.x;
+      leftWristY = results[0].pose.leftWrist.y;
+      console.log("leftWristX = "+ leftWristX + "leftWristY = " + leftWristY);
+
+    }
+}
+function draw(){
+    image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#8B0000");
+    circle(leftWristX, leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "volume = "+ volume;
+    song.setVolume(volume);
+}
+function preload(){
+    song = loadSound("Ed Sheeran - Bad Habits [Official].mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+}
